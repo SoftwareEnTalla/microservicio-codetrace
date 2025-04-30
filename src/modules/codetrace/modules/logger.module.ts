77c43ab4da1bd64617bd078e3765f87846f76971
Persy Morell Guerra <pmorell@xetid.cu>
2025-04-30 11:14:01 -0400

    Configuración completa del microservicio codetrace

--- src/modules/codetrace/modules/logger.module.ts
+++ src/modules/codetrace/modules/logger.module.ts
@@ -2,7 +2,7 @@
  * Copyright (c) 2025 SoftwarEnTalla
  * Licencia: MIT
  * Contacto: softwarentalla@gmail.com
- * CEOs:
+ * CEOs: 
  *       Persy Morell Guerra      Email: pmorellpersi@gmail.com  Phone : +53-5336-4654 Linkedin: https://www.linkedin.com/in/persy-morell-guerra-288943357/
  *       Dailyn García Domínguez  Email: dailyngd@gmail.com      Phone : +53-5432-0312 Linkedin: https://www.linkedin.com/in/dailyn-dominguez-3150799b/
  *
@@ -10,8 +10,8 @@
  * COO: Dailyn García Domínguez and Persy Morell Guerra
  * CFO: Dailyn García Domínguez and Persy Morell Guerra
  *
- * Repositories:
- *               https://github.com/SoftwareEnTalla
+ * Repositories: 
+ *               https://github.com/SoftwareEnTalla 
  *
  *               https://github.com/apokaliptolesamale?tab=repositories
  *
@@ -23,10 +23,11 @@
  *              https://www.facebook.com/profile.php?id=61572625716568
  *
  *              https://www.instagram.com/softwarentalla/
- *
+ *              
  *
  *
  */
+
 
 import { Module } from "@nestjs/common";
 import { HttpLoggerClient } from "src/common/logger/http-logger.client";
@@ -53,3 +54,4 @@ dotenv.config();
   exports: [LoggerClient],
 })
 export class LoggingModule {}
+
